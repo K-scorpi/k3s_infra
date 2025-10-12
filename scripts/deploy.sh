3d5c49304a64310c3a36fa3e43656d1b95b321f2
K-scorpi <mkirill602@gmail.com>
2025-10-12 21:48:55 +0500

    ci integration

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -77,9 +77,3 @@ case "${1:-all}" in
     usage
     ;;
 esac
-
-
-kubectl create secret generic telegram-bot-secret-2 \
-  -n bots \
-  --from-literal=token='8341782773:AAEB6unmAxqveRwBSHLVGLBVetoT74wn4Z4' \
-  --from-literal=chat_id='1202444249'
